840f6b697559b0d81828d3d54c98505e4cf45e79
TarikuGitHub <tarikukussia2010@gmail.com>
2023-06-19 12:15:07 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,16 @@ int main(void)
         srand(time(0));
         n = rand() - RAND_MAX / 2;
         if (n > 0)
-	printf("%d is positive\n", n);
+	{
+		printf("%d is %s\n", n, "postitive");
+	}
         else if (n == 0)
-	printf("%d is zero\n", n);    
+	{
+		printf("%d is %s\n", n, "zer");
+	}	
         else
-	printf("%d is negative\n", n);
+	{
+		printf("%d is %s\n", n, "negative");
+	}
 	return (0);
 }
