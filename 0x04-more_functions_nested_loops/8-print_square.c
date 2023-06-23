b7dbdb3b79688667becb61aca5e99a58a827ef74
TarikuGitHub <tarikukussia2010@gmail.com>
2023-06-23 10:15:19 -0700

    we

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,7 +6,7 @@
  */	
 void print_square(int size)
 {
-	if (size <= 0)
+	if(size <= 0)
 	{
 		_putchar('\n');
 	} else
@@ -17,9 +17,9 @@ void print_square(int size)
 		{
 			for (j = 0; j < size; j++)
 			{
-				_putchar('#');
-		        }
-           	        _putchar('\n');		
-      		 }
-	 }
+				_putchar('#')
+			}
+			_putchar('\n');		
+		}
+	}
 }
