91a056376130d896891669973a51ae660cb06d97
TarikuGitHub <tarikukussia2010@gmail.com>
2023-06-23 03:04:29 -0700

    Print a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers -checks for checks for a digit (0 through 9).
+ * print_numbers - checks for checks for a digit (0 through 9).
  *
  * Return: Always 0.
  */
