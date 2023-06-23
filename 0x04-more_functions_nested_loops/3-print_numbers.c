be934cdc9e16bcc0de1dff6cddf7d07a8ec774c3
TarikuGitHub <tarikukussia2010@gmail.com>
2023-06-23 03:09:33 -0700

    Print a new liclear

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+
 /**
  * print_numbers - checks for checks for a digit (0 through 9).
  *
@@ -7,10 +9,12 @@
 void print_numbers(void)
 {
 	int c;
-	for (c = 0; c < 10; c++)
+
+	for (c = 48; c < 50; c++)
 	{
 		_putchar(c);
 	}
+
 	_putchar('\n');
 }
 
