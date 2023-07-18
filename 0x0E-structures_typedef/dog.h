e5b3b0434e77daa67ebf8241b89d9481a38c00bd
TarikuGitHub <tarikukussia2010@gmail.com>
2023-07-18 05:19:07 -0700

    We

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * struct dog - Struct
- * @name: member
- * @age: member
- * @owner: member
- * Description: Define a new type
+ * struct dog - dog data
+ * @name: dog name
+ * @age: age in years
+ * @owner: name of guardian
+ *
+ * Description: example dog-themed struct for C project 0x0E.
  */
 struct dog
 {
@@ -13,15 +15,15 @@ struct dog
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
-/**
- * typedef struct dog_t dog - Struct
- * Description: Define a new type dog_t as a new name for the type struct dog
- */
+
 typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
+
 void free_dog(dog_t *d);
-#endiif
+
+#endif /* DOG_H */
