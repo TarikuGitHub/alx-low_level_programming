97433d441db70036989064bdb1f01d4922805548
TarikuGitHub <tarikukussia2010@gmail.com>
2023-07-18 01:20:44 -0700

    we

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,15 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
- * free_dog - function that frees dogs.
- * @d: input
- * Return: output
+ * free_dog - frees memory of structure dogs
+ * @d: pointer of structure
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != 0)
 	{
 		free(d->name);
 		free(d->owner);
